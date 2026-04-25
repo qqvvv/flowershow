0df373ecdacac06ae02147222afa0934f742bdd2
qqvvv <34702774+qqvvv@users.noreply.github.com>
2026-04-25 20:13:52 +0800

    Add configuration file for blog settings

---
+++ content/config.mjs
@@ -0,0 +1,16 @@
+const config = {
+  // title will be displayed on the top of your site
+  title: "My Awesome Blog",
+  // adding a description helps with SEO
+  description: "This is my awesome blog built with Flowershow",
+  // author of site displayed on the bottom of your site
+  author: "John",
+  // logo image
+  authorLogo: "/images/logo.svg",
+  // url to author website
+  domain: "https://john.app/",
+  // links to the pages you want to link to in the navbar
+  navLinks: [{ href: "/about", name: "About" }],
+};
+
+export default config;
